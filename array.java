157d8f84ff1ffc4d8c18630650e168ce12eae832
Pavithra <pavimanjunath62@gmail.com>
2026-08-28 08:55:02 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,20 @@
+public class array{
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+        System.out.println(sum);
+
+        double avg = (double) sum / arr.length;
+        System.out.println("Average: " + avg);
+
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+
+        }
+    }
+}
